d1fc104d3c4d245e6176228a5c92a5de3e18c5b1
cosito <cubillagonzalo19@gmail.com>
2024-07-29 03:35:37 -0300

    settings button js at script.js has been added

--- src/script.js
+++ src/script.js
@@ -1,1 +1,28 @@
-//HACER DESPUES
+const noSettings = document.getElementById('notSettings')
+const settings = document.getElementById('settings')
+const openSettingsBtn = document.getElementById('openBtn')
+const closeSettingsBtn = document.getElementById('closeBtn')
+
+const openSettings = () => {
+    //quitar todo
+    noSettings.classList.add('hidden')
+
+    //poner Settings
+    settings.classList.add('block')
+    settings.classList.remove('hidden')
+}
+const closeSettings = () => {
+    //quitar settings
+    noSettings.classList.remove('hidden')
+
+    //poner todo
+    settings.classList.add('hidden')
+    settings.classList.remove('block')
+}
+
+
+openSettingsBtn.addEventListener("click", openSettings)
+closeSettingsBtn.addEventListener("click", closeSettings)
+
+
+//crear funcion que revise el botón
