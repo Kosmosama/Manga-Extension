a8230d12b3f5b9e854b01c37a2660ef9fc56c1ba
Kosmosama <luismg0408@gmail.com>
2024-08-08 21:31:17 +0200

    Demo: (Todo) LogIn/LogOut

--- Demo/cargarMangas.js
+++ Demo/cargarMangas.js
@@ -1,9 +1,13 @@
 function cargarMangas() {
-    const storedMangas = localStorage.getItem('mangas');
-    if (storedMangas) {
-        mangas = JSON.parse(storedMangas);
+    if (isAuthorized) {
+        loadFromDrive();
+    } else {
+        const storedMangas = localStorage.getItem('mangas');
+        if (storedMangas) {
+            mangas = JSON.parse(storedMangas);
+        }
+        actualizarLista(); // Aplicar filtros y ordenamiento al cargar los datos
     }
-    actualizarLista(); // Aplicar filtros y ordenamiento al cargar los datos
 }
 
 document.addEventListener("DOMContentLoaded", cargarMangas);
--- Demo/guardarMangas.js
+++ Demo/guardarMangas.js
@@ -1,5 +1,8 @@
 function guardarMangas() {
     localStorage.setItem('mangas', JSON.stringify(mangas));
+    if (isAuthorized) {
+        saveToDrive();
+    }
 }
 
 window.addEventListener("beforeunload", guardarMangas);
