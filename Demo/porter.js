da816fdd502c42435e6713f67fada4524294f1a5
Kosmosama <luismg0408@gmail.com>
2024-08-10 19:52:34 +0200

    Demo: Import adjustments

--- Demo/porter.js
+++ Demo/porter.js
@@ -16,16 +16,17 @@ function exportar() {
 
 document.getElementById('import').addEventListener('change', function(event) {
     const file = event.target.files[0];
-    if (file && file.name.startsWith('mangas_') && file.name.endsWith('.json')) {
+    if (file && file.name.endsWith('.json')) {
         const reader = new FileReader();
         reader.onload = function(e) {
             try {
                 const importedMangas = JSON.parse(e.target.result);
-                if (Array.isArray(importedMangas)) {
+                if (Array.isArray(importedMangas) && importedMangas.every(validateMangaObject)) {
                     mangas.push(...importedMangas);
-                    actualizarLista();
+                    console.log('Imported mangas:', importedMangas);
+                    console.log('Current mangas array:', mangas);
                 } else {
-                    console.error('The file does not contain a valid array.');
+                    console.error('The file does not contain a valid mangas array.');
                 }
             } catch (error) {
                 console.error('Error parsing JSON:', error);
@@ -33,6 +34,21 @@ document.getElementById('import').addEventListener('change', function(event) {
         };
         reader.readAsText(file);
     } else {
-        console.error('Please select a valid mangas_*.json file.');
+        console.error('Please select a valid JSON file.');
     }
 });
+
+// Comprobation so each pushed manga has these keys
+function validateMangaObject(obj) {
+    const requiredKeys = [
+        'nombre',
+        'link',
+        'linkImagen',
+        'numCapitulos',
+        'favorito',
+        'fechaAdicion',
+        'ultimaLectura'
+    ];
+
+    return requiredKeys.every(key => obj.hasOwnProperty(key));
+}
