388a15835b3e425c927e5ba03f42e9e75ec41e57
Keys4Ever <cubillagonzalo@outlook.com>
2024-08-10 19:31:23 -0300

    Done: Add a button to the search bar to clear it (for when using only the mouse).

--- src/searchBar.js
+++ src/searchBar.js
@@ -1,8 +1,15 @@
 document.addEventListener('DOMContentLoaded', function() {
     const searchBar = document.getElementById('searchBar');
+    const deleteSearch = document.getElementById('deleteSearch')
     searchBar.addEventListener('input', function() {
         const query = this.value.toLowerCase();
         const resultados = mangaList.filter(manga => manga.title.toLowerCase().includes(query));
         cargarMangas(resultados);
     });
+    deleteSearch.addEventListener('click', ()=>{
+        document.getElementById('searchBar').value = "";
+        cargarMangas(mangaList);
+    })
+    
+
 });
