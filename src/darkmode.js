d3682188f95919e56560a61ec68afa3fb290afdf
Keys4Ever <cubillagonzalo@outlook.com>
2024-08-31 22:20:26 -0300

    i'm now addicted to classList.toggle

--- src/darkmode.js
+++ src/darkmode.js
@@ -1,17 +1,9 @@
 document.getElementById('darkmode').addEventListener('change', function(event) {
-    handleDarkMode(event.target.checked);
+
+    document.querySelector('html').classList.toggle('dark');
     saveCheckbox(event.target.checked);
 });
 loadCheckbox();
-
-function handleDarkMode(isChecked) {
-    const html = document.querySelector('html');
-    if (isChecked) {
-        html.classList.add('dark');
-    } else {
-        html.classList.remove('dark');
-    }
-}
 
 function saveCheckbox(isChecked) {
     chrome.storage.local.set({ darkmode: isChecked });
@@ -21,6 +13,7 @@ function loadCheckbox() {
     chrome.storage.local.get('darkmode', function(result) {
         const isChecked = result.darkmode;
         document.getElementById('darkmode').checked = isChecked;
-        handleDarkMode(isChecked);
+        const html = document.querySelector('html');
+        isChecked ? html.classList.toggle('dark') : html.classList.remove('dark');
     });
 }
