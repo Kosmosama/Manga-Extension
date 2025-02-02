ed22c841d886f647a5cd0d03af0f59c33cf29398
Kosmosama <luismg0408@gmail.com>
2025-02-03 00:03:53 +0100

    Fix: Zoneless is now properly applied.

--- main/src/app/app.config.ts
+++ main/src/app/app.config.ts
@@ -1,10 +1,10 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, provideExperimentalZonelessChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
     providers: [
-        provideZoneChangeDetection(),
+        provideExperimentalZonelessChangeDetection(),
         provideRouter(routes)]
 };
