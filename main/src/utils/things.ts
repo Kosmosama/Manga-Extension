463f1ce3fc9cb29840f30a9d17c9abe7f52ffc09
Keys4Ever <cubillagonzalo@outlook.com>
2025-02-07 00:58:01 -0300

    removed a thing that cursor did

--- main/src/utils/things.ts
+++
@@ -1,2 +0,0 @@
-export const EMangaType = ["manga", "manhwa", "manhua", "webcomic", "novel", "book", "one-shot", "doujinshi", "other"]
-export const EMangaState = ["reading", "completed", "on-hold", "dropped", "plan-to-read", "hiatus", "none"]
