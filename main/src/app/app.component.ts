b2671efd604a0892b4d6b03dc347b219c3651997
Keys4Ever <cubillagonzalo@outlook.com>
2025-02-11 14:04:18 -0300

    ⚡Feat: Add manga form for test things

--- main/src/app/app.component.ts
+++ main/src/app/app.component.ts
@@ -1,12 +1,39 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, inject } from '@angular/core';
+import { NonNullableFormBuilder, ReactiveFormsModule } from '@angular/forms';
+import { MangaType, MangaState, NewManga } from './shared/interfaces/manga.interface';
+import { MangaService } from './manga/services/manga.service';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [ReactiveFormsModule],
+  standalone: true,
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'main';
+  public manga = inject(MangaService);
+  #fb = inject(NonNullableFormBuilder);
+
+  mangaForm = this.#fb.group({
+    title: [''],
+    chapters: [0],
+    updatedAt: [new Date().toISOString()],
+    createdAt: [new Date().toISOString()],
+    link: [''],
+    image: [''],
+    type: [MangaType.Manga],
+    state: [MangaState.None],
+    isFavorite: [false],
+    tags: [[]]
+  });
+
+  saveManga() {
+    const newManga: NewManga = {
+      ...this.mangaForm.getRawValue()
+    };
+
+    this.manga.addManga(newManga).subscribe(() => {
+      this.mangaForm.reset();
+    });
+  }
 }
