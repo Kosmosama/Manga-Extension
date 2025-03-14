01da03b087079844708ca55b1e4b365fb1de9418
Kosmosama <luismg0408@gmail.com>
2025-03-14 23:58:56 +0100

    ⚡Feat: Implement tag form modal for adding and editing tags. (form not made yet)

--- main/src/app/features/tags/tag-form/tag-form.component.ts
+++ main/src/app/features/tags/tag-form/tag-form.component.ts
@@ -1,11 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, input, output } from '@angular/core';
+import { Tag } from '../../../core/interfaces/tag.interface';
 
 @Component({
-  selector: 'app-tag-form',
-  imports: [],
-  templateUrl: './tag-form.component.html',
-  styleUrl: './tag-form.component.css'
+    selector: 'tag-form',
+    imports: [],
+    templateUrl: './tag-form.component.html',
+    styleUrl: './tag-form.component.css'
 })
 export class TagFormComponent {
+    tag = input<Tag | null>();
 
+    formSumbitted = output<Tag>();
 }
--- main/src/app/features/tags/tags-page/tags-page.component.ts
+++ main/src/app/features/tags/tags-page/tags-page.component.ts
@@ -1,18 +1,23 @@
-import { Component, effect, inject, signal } from '@angular/core';
+import { Component, effect, inject, signal, viewChild } from '@angular/core';
 import { TagService } from '../../../core/services/tag.service';
 import { Tag } from '../../../core/interfaces/tag.interface';
 import { TagCardComponent } from '../tag-card/tag-card.component';
+import { ModalComponent } from '../../../shared/modal/modal.component';
+import { TagFormComponent } from '../tag-form/tag-form.component';
 
 @Component({
     selector: 'app-tags-page',
-    imports: [TagCardComponent],
+    imports: [TagCardComponent, ModalComponent, TagFormComponent],
     templateUrl: './tags-page.component.html',
     styleUrl: './tags-page.component.css'
 })
 export class TagsPageComponent {
     private tagService = inject(TagService);
 
+    modal = viewChild.required<ModalComponent>('modal');
+
     tagList = signal<Tag[]>([]);
+    selectedTag = signal<Tag | null>(null);
 
     ngOnInit() {
         effect(() => {
@@ -28,5 +33,30 @@ export class TagsPageComponent {
      */
     handleTagDeletion(id: number) {
         this.tagList.update((tags) => tags.filter(tag => tag.id !== id));
+    }
+
+    /**
+     * Opens the modal to create or edit a tag.
+     */
+    openForm(tag?: Tag) {
+        this.selectedTag.set(tag || null);
+        this.modal().open();
+    }
+
+    /**
+     * Handles the form submission.
+     * 
+     * @param tag The tag to add or update.
+     */
+    handleFormSumbission(tag: Tag) {
+        this.tagList.update((tags) => {
+            const index = tags.findIndex(m => m.id === tag.id);
+            if (index === -1) {
+                return [...tags, tag];
+            } else {
+                tags[index] = tag;
+                return tags;
+            }
+        });
     }
 }
