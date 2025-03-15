3d02e1e81ced17db5e8658c33c8c93e5da05ca67
Kosmosama <luismg0408@gmail.com>
2025-03-15 20:50:44 +0100

    ⚡Feat: Tag initial form.

--- main/src/app/features/tags/tag-form/tag-form.component.ts
+++ main/src/app/features/tags/tag-form/tag-form.component.ts
@@ -1,14 +1,52 @@
-import { Component, input, output } from '@angular/core';
+import { Component, inject, input, output } from '@angular/core';
 import { Tag } from '../../../core/interfaces/tag.interface';
+import { TagService } from '../../../core/services/tag.service';
+import { NonNullableFormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
     selector: 'tag-form',
-    imports: [],
+    imports: [ReactiveFormsModule],
     templateUrl: './tag-form.component.html',
     styleUrl: './tag-form.component.css'
 })
 export class TagFormComponent {
+    private tagService = inject(TagService);
+    private fb = inject(NonNullableFormBuilder);
+
     tag = input<Tag | null>();
 
     formSumbitted = output<Tag>();
+
+    tagForm = this.fb.group({
+        name: ["", [Validators.required, Validators.minLength(3)]],
+        color: ["#ffffff"], //#TODO Color wheel
+    });
+
+    ngOnInit() {
+        if (this.tag()) {
+            this.tagForm.patchValue({
+                name: this.tag()!.name,
+                color: this.tag()!.color || "#ffffff",
+            });
+            this.tagForm.markAllAsTouched();
+        }
+    }
+
+    // #TODO make it put when editing
+    submit() {
+        if (this.tagForm.invalid) return;
+
+        const newTag: Tag = {
+            ...this.tagForm.getRawValue(),
+            id: Number(`${Date.now()}${Math.floor(Math.random() * 10000)}`)
+        };
+
+        this.tagService.addTag(newTag).subscribe({
+            next: () => {
+                this.formSumbitted.emit(newTag);
+                console.log("Tag added successfully");
+            },
+            error: (err) => console.error("Errpr:", err)
+        });
+    }
 }
