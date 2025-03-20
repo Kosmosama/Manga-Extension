609353f3e72c6781331d14f183de88e04551ab41
Kosmosama <luismg0408@gmail.com>
2025-03-20 23:44:37 +0100

    ⚡Feat: Implement language initialization and storage in SettingsService.

--- main/src/app/core/services/settings.service.ts
+++ main/src/app/core/services/settings.service.ts
@@ -1,20 +1,23 @@
 import { inject, Injectable, OnInit, signal } from '@angular/core';
-import { TranslocoService } from '@jsverse/transloco';
+import { TranslocoService, getBrowserLang } from '@jsverse/transloco';
 
 @Injectable({
     providedIn: 'root'
 })
 export class SettingsService implements OnInit {
     private translocoService = inject(TranslocoService);
+    private readonly STORAGE_KEY = 'preferredLanguage';
 
-    readonly languages = signal(this.translocoService.getAvailableLangs());
-    readonly activeLanguage = signal(this.translocoService.getDefaultLang());
+    readonly languages = signal(this.translocoService.getAvailableLangs() as string[]);
+    readonly activeLanguage = signal('');
 
     constructor() {
         console.log('SettingsService created');
     }
 
     ngOnInit() {
+        this.initializeLanguage();
+
         this.translocoService.langChanges$.subscribe(lang => {
             this.activeLanguage.set(lang);
         });
@@ -23,7 +26,52 @@ export class SettingsService implements OnInit {
         console.log(this.activeLanguage());
     }
 
+    /**
+     * Initialize the language based on the stored value, browser language or default language.
+     */
+    private initializeLanguage() {
+        chrome.storage.local.get(this.STORAGE_KEY, (result) => {
+            const storedLang = result[this.STORAGE_KEY];
+            if (storedLang && this.languages().includes(storedLang)) {
+                this.setLanguage(storedLang);
+            }
+            else {
+                const browserLang = getBrowserLang() || '';
+                if (this.languages().includes(browserLang)) {
+                    this.setLanguage(browserLang);
+                }
+                else {
+                    this.setLanguage(this.translocoService.getDefaultLang());
+                }
+            }
+        });
+    }
+
+    /**
+     * Change the language to the specified value.
+     * 
+     * @param {string} value The language to change to.
+     */
     changeLanguage(value: string) {
-        this.translocoService.setActiveLang(value);
+        if (this.languages().includes(value)) {
+            this.setLanguage(value);
+        }
+        else {
+            console.warn(`Language ${value} is not available.`);
+        }
+    }
+
+    /**
+     * Save the language to the storage and set it as active.
+     * 
+     * @param {string} lang The language to save.
+     */
+    private setLanguage(lang: string) {
+        chrome.storage.local.set({ [this.STORAGE_KEY]: lang }, () => {
+            console.log(`Language saved: ${lang}`);
+        });
+
+        this.translocoService.setActiveLang(lang);
+        this.activeLanguage.set(lang);
     }
 }
